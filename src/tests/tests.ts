a546d3079929bbc31bb86bc43f6bb51d06431633
Sivak <abounding05@gmail.com>
2020-01-27 12:27:20 -0500

    havent committed in a while

--- src/tests/tests.ts
+++
@@ -1,9 +0,0 @@
-import startServer from "../index";
-
-const _server = startServer();
-
-async function testDefault(): Promise<Boolean> {
-  const server = await _server;
-  const response = await fetch(server.endpoint("test")); // 'localhost:8080/test'
-  return response.ok;
-}
