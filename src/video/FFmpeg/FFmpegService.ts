bea3ecb0b82b4c2d03ef81dd20fcfea924befbb5
Corrina Sivak <abounding05@gmail.com>
2020-02-25 11:12:30 -0500

    Created a video conversion service

--- src/video/FFmpeg/FFmpegService.ts
+++ src/video/FFmpeg/FFmpegService.ts
@@ -1,6 +1,6 @@
 /**
  * @author Corrina Sivak
- * @description A service built to abstract the use of FFMpeg
+ * @description A service built to abstract the use of FFmpeg
  */
 
 import env from "env-var";
@@ -9,26 +9,48 @@ import os from "os";
 import uuid from "uuid";
 import fs from "fs";
 import path from "path";
+import stream from "stream";
 
-const FFMPEG_PATH = "/Users/sivak/Documents/projects/RemixServer/.ignored/ffmpeg"; //env.get('FFMPEG_PATH').required().asString();
+const FFMPEG_PATH = env
+  .get("FFMPEG_PATH")
+  .required()
+  .asString();
 
+/**
+ * Converts a video using ffmpeg to a universally readable format, and compresses the bitrate.
+ * @param video_path The path to the uploaded video to be converted.
+ * @returns A promise of the video's read stream.
+ * This read stream automatically deletes both the original and the converted file once it's done being read.
+ */
 function convert_video(video_path: string) {
-  const output_filename = uuid() + ".mp4";
-  return new Promise((resolve, reject) => {
-    child_process
-      .spawn(FFMPEG_PATH, ["-loglevel fatal", `-i ${video_path}`, output_filename], {
+  const output_path = path.join(os.tmpdir(), uuid() + ".mp4");
+  if (process.env.NODE_ENV === "development") {
+    console.log(`outputting ${video_path} to ${output_path}`);
+  }
+  return new Promise<stream.Readable>((resolve, reject) => {
+    const cp = child_process
+      .spawn(FFMPEG_PATH, ["-i", video_path, output_path], {
         windowsHide: true,
       })
-      .on("exit", () => {
-        //resolve(fs.createReadStream(path.join(os.tmpdir(), output_filename)));
-        resolve(path.join(os.tmpdir(), output_filename));
+      .on("close", status => {
+        if (status === 0) {
+          const read_stream = fs.createReadStream(output_path).on("close", () => {
+            fs.unlinkSync(output_path);
+            if (process.env.NODE_ENV === "production") {
+              // In dev we want to save the original file so that we can retest.
+              fs.unlinkSync(video_path);
+            }
+          });
+          resolve(read_stream);
+        } else {
+          reject(new Error(`FFmpeg process failed with exit code ${status}. The input video probably doesn't exist.`));
+        }
       })
       .on("error", err => {
-        reject(err);
+        reject(new Error(err.message));
       });
+    if (process.env.NODE_ENV === "development") {
+      cp.stderr.pipe(process.stderr);
+    }
   });
 }
-
-convert_video("~/Downloads/Snapchat-523608777(1).mp4")
-  .then(console.log)
-  .catch(console.trace);
